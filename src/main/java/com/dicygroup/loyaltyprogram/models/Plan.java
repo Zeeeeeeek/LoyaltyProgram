00d89b1b39fe1e628ad891d9d1e03142866f3110
Enrico Ulissi <enricoulissi01@gmail.com>
2023-06-02 10:31:48 +0200

    add json subtypes

--- src/main/java/com/dicygroup/loyaltyprogram/models/Plan.java
+++ src/main/java/com/dicygroup/loyaltyprogram/models/Plan.java
@@ -1,5 +1,13 @@
 package com.dicygroup.loyaltyprogram.models;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = PointsPlan.class, name = "points"),
+        @JsonSubTypes.Type(value = LevelsPlan.class, name = "levels")
+})
 public interface Plan {
 
     String getOwner();
