818d065c3206a1ea26ab899976d18833430c97a8
Enrico Ulissi <enricoulissi01@gmail.com>
2023-06-02 10:41:04 +0200

    add plan persistence

--- src/main/java/com/dicygroup/loyaltyprogram/models/plans/AbstractPlan.java
+++ src/main/java/com/dicygroup/loyaltyprogram/models/plans/AbstractPlan.java
@@ -1,10 +1,18 @@
 package com.dicygroup.loyaltyprogram.models.plans;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @AllArgsConstructor
+@NoArgsConstructor
 public abstract class AbstractPlan implements Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Getter
     private Long id;
 
