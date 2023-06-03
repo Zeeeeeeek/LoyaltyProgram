bb0770cc7cf02e4ed832a7dd0612a27267c7af9f
Enrico Ulissi <enricoulissi01@gmail.com>
2023-06-03 11:52:57 +0200

    add owner id into a request path

--- src/main/java/com/dicygroup/loyaltyprogram/interfaces/ShopkeeperInterface.java
+++ src/main/java/com/dicygroup/loyaltyprogram/interfaces/ShopkeeperInterface.java
@@ -3,9 +3,11 @@ package com.dicygroup.loyaltyprogram.interfaces;
 import com.dicygroup.loyaltyprogram.managers.PlanManager;
 import com.dicygroup.loyaltyprogram.models.plans.AbstractPlan;
 import com.dicygroup.loyaltyprogram.models.plans.Plan;
+import com.dicygroup.loyaltyprogram.models.shopkeepers.Shopkeeper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +26,13 @@ public class ShopkeeperInterface {
         return planManager.getPlanList();
     }
 
-    @PostMapping("plans")
-    public Plan create(@RequestBody AbstractPlan plan) {
+    @PostMapping("{ownerId}/plans")
+    public Plan create(@RequestBody AbstractPlan plan, @PathVariable Long ownerId) {
+       plan.setOwner(getShopKeeperFromId(ownerId));
        return planManager.savePlan(plan);
+    }
+
+    private Shopkeeper getShopKeeperFromId(Long id) {
+        return new Shopkeeper("pippo", "pluto"); //temporary method
     }
 }
