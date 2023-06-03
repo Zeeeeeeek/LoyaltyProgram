2b452582b629155d0028e045921062d16f08ae35
Enrico Ulissi <enricoulissi01@gmail.com>
2023-06-03 11:41:00 +0200

    replace multiple import

--- src/main/java/com/dicygroup/loyaltyprogram/models/plans/AbstractPlan.java
+++ src/main/java/com/dicygroup/loyaltyprogram/models/plans/AbstractPlan.java
@@ -2,7 +2,14 @@ package com.dicygroup.loyaltyprogram.models.plans;
 
 import com.dicygroup.loyaltyprogram.models.plans.rules.PointRule;
 import com.dicygroup.loyaltyprogram.models.shopkeepers.Shopkeeper;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
