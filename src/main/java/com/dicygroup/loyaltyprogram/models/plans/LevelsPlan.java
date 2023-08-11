28c08c2cc5378d3611b426096e7fd4ed2f4e92e4
Enrico Ulissi <enricoulissi01@gmail.com>
2023-08-11 19:36:53 +0200

    add LevelsPlan fields and methods

--- src/main/java/com/dicygroup/loyaltyprogram/models/plans/LevelsPlan.java
+++ src/main/java/com/dicygroup/loyaltyprogram/models/plans/LevelsPlan.java
@@ -2,13 +2,24 @@ package com.dicygroup.loyaltyprogram.models.plans;
 
 import com.dicygroup.loyaltyprogram.models.plans.rules.PointRule;
 import jakarta.persistence.Entity;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@Getter
+@Setter
 public class LevelsPlan extends AbstractPlan {
+
+    private int levelNumber;
+    private int pointsPerLevel;
 
     public LevelsPlan(PointRule pointRule, boolean isOpenToCoalition) {
         super(pointRule, isOpenToCoalition);
+    }
+
+    public int evalLevel(int points) {
+        return points / pointsPerLevel;
     }
 }
