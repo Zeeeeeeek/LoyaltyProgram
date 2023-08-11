52e42626f35b6fb8df3ba3ff21ca2ff33fe25b22
Enrico Ulissi <enricoulissi01@gmail.com>
2023-08-11 15:28:13 +0200

    refactor Rule

--- src/test/java/com/dicygroup/loyaltyprogram/models/plans/rules/PointRulePlanTest.java
+++ src/test/java/com/dicygroup/loyaltyprogram/models/plans/rules/PointRulePlanTest.java
@@ -9,44 +9,37 @@ class PointRulePlanTest {
 
     @Test
     void ruleShouldBeCreated() {
-        PointRule pointRule = new PointRule(1.0);
+        PointRule pointRule = new PointRule(1);
         assertEquals(1.0, pointRule.getPointValueInCurrency());
     }
 
     @Test
     void ruleShouldNotBeCreatedWithNegativeValue() {
-        assertThrows(IllegalArgumentException.class, () -> new PointRule(-1.0));
+        assertThrows(IllegalArgumentException.class, () -> new PointRule(-1));
     }
 
     @Test
     void ruleShouldNotBeCreatedWithZeroValue() {
-        assertThrows(IllegalArgumentException.class, () -> new PointRule(0.0));
-    }
-
-    @Test
-    void ruleShouldNotBeCreatedWithNullValue() {
-        assertThrows(NullPointerException.class, () -> new PointRule(null));
+        assertThrows(IllegalArgumentException.class, () -> new PointRule(0));
     }
 
     @Test
     void ruleShouldBeApplied() {
-        PointRule pointRule = new PointRule(1.0);
+        PointRule pointRule = new PointRule(1);
         assertEquals(1, pointRule.apply(1.0));
-        pointRule = new PointRule(2.0);
+        pointRule = new PointRule(2);
         assertEquals(2, pointRule.apply(4.0));
-        pointRule = new PointRule(0.5);
-        assertEquals(2, pointRule.apply(1.0));
     }
 
     @Test
     void ruleShouldNotBeAppliedWithNegativeAmount() {
-        PointRule pointRule = new PointRule(1.0);
+        PointRule pointRule = new PointRule(1);
         assertThrows(IllegalArgumentException.class, () -> pointRule.apply(-1.0));
     }
 
     @Test
     void ruleShouldNotBeAppliedWithNullAmount() {
-        PointRule pointRule = new PointRule(1.0);
+        PointRule pointRule = new PointRule(1);
         assertThrows(NullPointerException.class, () -> pointRule.apply(null));
     }
 }
