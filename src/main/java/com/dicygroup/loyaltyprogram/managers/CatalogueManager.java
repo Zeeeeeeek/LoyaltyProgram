0a9c226342b4d5625a3e6ccf4798f9a4567ab677
Enrico Ulissi <enricoulissi01@gmail.com>
2023-08-17 17:09:31 +0200

    fix plans accepting catalogues with invalid cost type

--- src/main/java/com/dicygroup/loyaltyprogram/managers/CatalogueManager.java
+++ src/main/java/com/dicygroup/loyaltyprogram/managers/CatalogueManager.java
@@ -1,11 +1,19 @@
 package com.dicygroup.loyaltyprogram.managers;
 
 import com.dicygroup.loyaltyprogram.models.plans.AbstractPlan;
+import com.dicygroup.loyaltyprogram.models.plans.LevelsPlan;
+import com.dicygroup.loyaltyprogram.models.plans.PointsPlan;
 import com.dicygroup.loyaltyprogram.models.plans.catalogues.Catalogue;
+import com.dicygroup.loyaltyprogram.models.plans.catalogues.Prize;
+import com.dicygroup.loyaltyprogram.models.plans.catalogues.costs.LevelCost;
+import com.dicygroup.loyaltyprogram.models.plans.catalogues.costs.PointCost;
 import com.dicygroup.loyaltyprogram.registries.CatalogueRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 @Slf4j
@@ -18,15 +26,36 @@ public class CatalogueManager {
     public boolean createCatalogue(Long planId, Catalogue catalogue) {
         AbstractPlan plan = planManager.getPlanById(planId);
         catalogue.setPlan(plan);
+        List<Prize> prizes = catalogue.getPrizes();
+        if (prizesAreNotCompatibleWithPlanType(plan, prizes)) return false;
         try {
-            if(catalogueRegistry.findByPlanId(planId) != null)
-                catalogueRegistry.delete(catalogueRegistry.findByPlanId(planId));
-            catalogueRegistry.save(catalogue);
+            safeSave(catalogue, planId);
             return true;
         } catch (Exception e) {
             log.error("Error while saving catalogue", e);
             return false;
         }
+    }
+
+    private void safeSave(Catalogue catalogue, Long planId) {
+        if (catalogueRegistry.findByPlanId(planId) != null)
+            catalogueRegistry.delete(catalogueRegistry.findByPlanId(planId));
+        catalogueRegistry.save(catalogue);
+    }
+
+    private boolean prizesAreNotCompatibleWithPlanType(AbstractPlan plan, List<Prize> prizes) {
+        if (plan instanceof PointsPlan) {
+            return notAllMatch(prizes, prize -> prize.getCost() instanceof PointCost);
+        }
+        return notAllMatch(prizes.stream()
+                .filter(prize -> prize.getCost() instanceof LevelCost)
+                .map(prize -> (LevelCost) prize.getCost())
+                .toList(),
+                levelCost -> levelCost.getRequiredLevel() <= ((LevelsPlan) plan).getLevelNumber());
+    }
+
+    private <T> boolean notAllMatch(List<T> prizes, Predicate<T> predicate) {
+        return !prizes.stream().allMatch(predicate);
     }
 
     public Catalogue getCatalogue(Long planId) {
