a8d4abc8da1d29860ebf74e83c93eb36f65f686b
Enrico Ulissi <enricoulissi01@gmail.com>
2023-08-17 15:07:20 +0200

    fix prize level cost

--- src/main/java/com/dicygroup/loyaltyprogram/managers/PrizeManager.java
+++ src/main/java/com/dicygroup/loyaltyprogram/managers/PrizeManager.java
@@ -1,10 +1,10 @@
 package com.dicygroup.loyaltyprogram.managers;
 
+import com.dicygroup.loyaltyprogram.models.plans.AbstractPlan;
+import com.dicygroup.loyaltyprogram.models.plans.LevelsPlan;
 import com.dicygroup.loyaltyprogram.models.plans.catalogues.Prize;
 import com.dicygroup.loyaltyprogram.models.plans.catalogues.costs.Cost;
 import com.dicygroup.loyaltyprogram.models.plans.catalogues.costs.LevelCost;
-import com.dicygroup.loyaltyprogram.models.plans.catalogues.costs.PointCost;
-import com.dicygroup.loyaltyprogram.models.subscription.Subscription;
 import com.dicygroup.loyaltyprogram.registries.PrizeRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,38 +14,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class PrizeManager {
+
     private final SubscriptionManager subscriptionManager;
     private final PrizeRegistry prizeRegistry;
+    private final PlanManager planManager;
 
     public Boolean pickUpPrize(Long prizeId, Long planId, Long customerId) {
         int actualPoint = subscriptionManager.getCustomerStatus(customerId, planId);
         Cost prizeCost = getPrize(prizeId).getCost();
-
-
-        if (prizeCost instanceof LevelCost){
-            if(actualPoint >= ((LevelCost) prizeCost).getRequiredLevel()){
-                if (actualPoint >= prizeCost.getRequiredPoints()) {
-                    return removePoints(actualPoint, prizeCost.getRequiredPoints(), customerId, planId);
-
-                }else throw new RuntimeException("Not enough points");
-
-            }else throw new RuntimeException("Not enough points");
-
-        } else if (prizeCost instanceof PointCost) {
-            if(actualPoint >= prizeCost.getRequiredPoints()){
-                return removePoints(actualPoint, prizeCost.getRequiredPoints(), customerId, planId);
-
-            }else throw new RuntimeException("Not enough points");
+        if (prizeCost instanceof LevelCost levelCost) {
+            AbstractPlan plan = planManager.getPlanById(planId);
+            if (plan instanceof LevelsPlan levelsPlan && (levelsPlan.evalLevel(actualPoint) < levelCost.getRequiredLevel())) {
+                return false;
+            }
         }
-
-        return false;
+        if (actualPoint < prizeCost.getRequiredPoints())
+            return false;
+        return removePoints(actualPoint, prizeCost.getRequiredPoints(), customerId, planId);
     }
 
-    private Boolean removePoints(Integer actualPoint, Integer prizeCost, Long customerId, Long planId){
-        Integer newPoints = actualPoint - prizeCost;
-        subscriptionManager.setPoints(customerId, planId, newPoints);
-        return true;
-
+    private Boolean removePoints(Integer actualPoint, Integer prizeCost, Long customerId, Long planId) {
+        return subscriptionManager.setPoints(customerId, planId, actualPoint - prizeCost);
     }
 
     public Prize getPrize(Long prizeId) {
